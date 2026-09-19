901b77463884a16c034d2bc2bf0658e84da03544
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-09-19 11:00:54 -0700

    Add shopping_cart function with products list

---
+++ shopping_cart.py
@@ -0,0 +1,10 @@
+def shopping_cart():
+    products = [
+        ("T-Shirt", 800),
+        ("Jeans", 1800),
+        ("Shoes", 2500),
+        ("Cap", 300),
+        ("Jacket", 3200),
+        ("Socks", 150),
+    ]
+    print("\n--- Welcome to the Shop ---")
