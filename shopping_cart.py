57f1f99c2b2db4eb79a3fe298bd4f14ddb3266a5
syedibrahimhamza <ibrahimhamza1211@gmail.com>
2026-09-19 11:06:25 -0700

    add prodcut display

--- shopping_cart.py
+++ shopping_cart.py
@@ -8,3 +8,8 @@ def shopping_cart():
         ("Socks", 150),
     ]
     print("\n--- Welcome to the Shop ---")
+    while True:
+        print("\nProducts:")
+        for i in range(len(products)):
+            name, price = products[i]
+            print(f"{i + 1}. {name} - Rs. {price}")
